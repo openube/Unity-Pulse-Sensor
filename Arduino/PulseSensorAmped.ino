e36ab0f13d7aabc874e359ae7fc23a25b035a700
Giorgio Pomettini <giorgio.freespire@gmail.com>
2015-02-18 00:28:35 +0100

    Arduino files added

---
+++ Arduino/PulseSensorAmped.ino
@@ -0,0 +1,31 @@
+int pulsePin = 0;
+int blinkPin = 13;
+
+volatile int BPM;
+volatile int Signal;
+volatile int IBI = 600;
+volatile boolean Pulse = false;
+volatile boolean QS = false;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(blinkPin, OUTPUT);
+
+  interruptSetup();
+}
+
+void loop()
+{
+  if (QS == true)
+  {
+    Serial.println(BPM);
+    QS = false;
+  }
+  else
+  {
+    Serial.println(0);
+  }
+
+  delay(20);
+}
